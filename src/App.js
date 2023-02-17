6aca371bed1786f7f28b4103dacccc8cacb82d1b
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-02-17 17:34:38 -0300

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
+
+
+const TURNS={
+  X:'X',
+  O:'O'
+}
+
+
+const GAME_COMBOS=[]
+
 
 function App() {
+  const [board, setBoard]=useState(Array(9).fill(null))
+
+  const[TURN, SetTurn]=useState(TURNS.X)
+
+  const clickHandler=(index)=>{  
+    updateBoard(index)
+    SetTurn(TURNS.X===TURN?TURNS.O:TURNS.X)
+
+  }
+  const updateBoard=(index)=>{
+    const newBoard=[...board]
+    newBoard[index]=TURN
+
+    setBoard(newBoard)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1>TIC TAC TOE</h1>
+
+        <div className="containerTic">
+            {
+              board.map((turn, index)=>{
+                return(
+                  <div key={index} className="square" onClick={()=>clickHandler(index)}>
+                    {turn}
+                  </div>
+                )
+              })
+            }
+        </div>
+        <div className="containerTurnInfo">
+          <label>Turn for:</label>
+          <div className="turnInfo" isSelected={TURN==='X'}>X</div>
+          <div className="turnInfo" isSelected={TURN==='O'}>O</div>
+
+        </div>
     </div>
   );
 }
